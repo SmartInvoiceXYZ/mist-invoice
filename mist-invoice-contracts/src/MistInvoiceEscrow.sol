f188f257dd4710db6eb29a7eb7b69545e7174c87
Jordan Walker <jordan@jordanwalker.us>
2023-08-01 11:59:46 -0500

    Laid out basic structure with TODOs for interactions needed

---
+++ mist-invoice-contracts/src/MistInvoiceEscrow.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+// TODO import ISmartInvoiceEscrow
+
+contract MistInvoiceEscrow {
+
+    address public immutable INVOICE_FACTORY;
+    address public mistPool;
+
+    mapping(address => MistSecret) mistSecrets;
+
+    // TODO how many bytes?
+    struct MistSecret {
+        bytes merkleRoot;
+        bytes encClientRandInt;
+        bytes encProviderRandInt;
+        bytes encClientKey;
+        bytes encProviderKey;
+    }
+
+    constructor(address _invoiceFactory, address _mistPool) public {
+        INVOICE_FACTORY = _invoiceFactory;
+        mistPool = _mistPool;
+    }
+
+    function createInvoice(bytes calldata _merkleRoot,
+                           bytes calldata _clientRandom,
+                           bytes calldata _providerRandom,
+                           bytes calldata _clientKey,
+                           bytes calldata _providerKey,
+                           uint256[] calldata _amounts,
+                           bytes calldata _data) external returns (address invoice) {
+        require(_merkleRoot != bytes(0), "merkle root required");
+        // TODO check other inputs
+
+        // TODO set tracking for client and provider
+        MistSecret memory meta = MistSecret(_merkleRoot, _clientRandom, _providerRandom, _clientKey, _providerKey);
+        // TODO call factory.createInvoice
+        // TODO add meta to mapping for invoice address
+
+        // TODO return invoice address
+    }
+
+    function privateDeposit(address _invoiceAddr) external {
+        // TODO call MIST withdraw()
+        // TODO call transfer to _invoiceAddr
+    }
+
+    function privateRelease(address _invoiceAddr, bytes calldata _proof, uint256 _milestone) {
+        // TODO verify proof
+        // TODO call release on _invoiceAddr
+        // TODO save amount to provider data
+        // TODO deposit to MIST pool
+    }
+
+    function privateDispute(address _invoiceAddr, bytes calldata _proof) {
+        // TODO verify proof
+        // TODO call lock on _invoiceAddr
+    }
+
+    // TODO other resolve arguments needed
+    function resolve(address _invoiceAddr) {
+        // TODO check arbitration address msg.sender
+        // TODO calculate client, provider, arb splits; update mappings
+        // TODO call invoice resolve
+        // TODO transfer arb fee directly to arb
+        // TODO deposit to MIST pool for client + provider
+    }
+
+    function privateWithdraw(address _invoiceAddr, bytes calldata _proof) {
+        // TODO verify withdraw enabled
+        // TODO verify proof of client
+        // TODO call withdraw on invoice
+        // TODO update mapping
+        // TODO call deposit on MIST pool
+    }
+}
