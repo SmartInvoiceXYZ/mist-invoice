00da734c62dad6badebf87d340f5a0a38fcacbd1
Jordan Walker <jordan@jordanwalker.us>
2023-08-12 19:19:17 -0500

    Roughed in the calls, leaving out MIST interactions for now

--- mist-invoice-contracts/src/MistInvoiceEscrowWrapper.sol
+++ mist-invoice-contracts/src/MistInvoiceEscrowWrapper.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.13;
 
 import "./interfaces/ISmartInvoiceFactory.sol";
+import "./interfaces/ISmartInvoiceEscrow.sol";
 import "./interfaces/IMistPool.sol";
 
 contract MistInvoiceEscrowWrapper {
@@ -65,32 +66,67 @@ contract MistInvoiceEscrowWrapper {
         // TODO call transfer to _invoiceAddr
     }
 
+    // TODO is reentrency guard needed?
     function privateRelease(address _invoiceAddr, bytes calldata _proof, uint256 _milestone) external {
+        require(_invoiceAddr != address(0), "valid invoice required");
         // TODO verify proof
         // TODO call release on _invoiceAddr
+        ISmartInvoiceEscrow escrow = ISmartInvoiceEscrow(_invoiceAddr);
+        IERC20 token = IERC20(escrow.token());
+        uint256 preBalance = token.balanceOf(address(this));
+        escrow.release(_milestone);
+        uint256 postBalance = token.balanceOf(address(this));
         // TODO save amount to provider data
+        uint256 providerOwed = postBalance - preBalance;
         // TODO deposit to MIST pool
     }
 
-    function privateDispute(address _invoiceAddr, bytes calldata _proof) external {
+    function privateDispute(address _invoiceAddr, bytes32 _details, bytes calldata _proof) external {
+        require(_invoiceAddr != address(0), "valid invoice required");
         // TODO verify proof
         // TODO call lock on _invoiceAddr
+        ISmartInvoiceEscrow escrow = ISmartInvoiceEscrow(_invoiceAddr);
+        escrow.lock(_details);
     }
 
     // TODO other resolve arguments needed
-    function resolve(address _invoiceAddr) external {
+    function resolve(address _invoiceAddr, uint256 _clientAward, uint256 _providerAward, bytes32 _details) external {
+        require(_invoiceAddr != address(0), "valid invoice required");
         // TODO check arbitration address msg.sender
         // TODO calculate client, provider, arb splits; update mappings
+        ISmartInvoiceEscrow escrow = ISmartInvoiceEscrow(_invoiceAddr);
+        IERC20 token = IERC20(escrow.token());
+        uint256 preBalance = token.balanceOf(address(this));
         // TODO call invoice resolve
-        // TODO transfer arb fee directly to arb
+        _invoiceAddr.delegatecall(
+            abi.encodeWithSelector(ISmartInvoiceEscrow.resolve.selector, _clientAward, _providerAward, _details)
+        );
+        uint256 postBalance = token.balanceOf(address(this));
+        // TODO save amount to provider data
+        uint256 totalOwed = postBalance - preBalance;
+        uint256 forClient = _clientAward / (_clientAward + _providerAward) * totalOwed;
+        uint256 forProvider = _providerAward / (_clientAward + _providerAward) * totalOwed;
         // TODO deposit to MIST pool for client + provider
+        // mistPool.deposit(forClient)
+        // mistPool.deposit(forProvider)
     }
 
+    // Only works for primary token, withdrawTokens is not implemented for hackathon
     function privateWithdraw(address _invoiceAddr, bytes calldata _proof) external {
+        require(_invoiceAddr != address(0), "valid invoice required");
         // TODO verify withdraw enabled
         // TODO verify proof of client
         // TODO call withdraw on invoice
+
+        ISmartInvoiceEscrow escrow = ISmartInvoiceEscrow(_invoiceAddr);
+        IERC20 token = IERC20(escrow.token());
+        uint256 preBalance = token.balanceOf(address(this));
+        escrow.withdraw();
+        uint256 postBalance = token.balanceOf(address(this));
+        uint256 owed = postBalance - preBalance;
         // TODO update mapping
         // TODO call deposit on MIST pool
+        // mistPool.deposit(owed)
     }
+
 }
