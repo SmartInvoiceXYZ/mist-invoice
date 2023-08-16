4fa7e260188dbb89d4ebf17f41888faff0d140ad
psparacino <tufnelenterprises@gmail.com>
2023-08-16 12:20:08 -0400

    adjusted structs, create invoice on sepolia working

--- mist-invoice-contracts/script/createMistInvoice.s.sol
+++ mist-invoice-contracts/script/createMistInvoice.s.sol
@@ -7,11 +7,36 @@ import "forge-std/console.sol";
 
 contract InvoiceCreationScript is Script {
     struct MistData {
-        bytes merkleRoot;
-        bytes clientRandom;
-        bytes providerRandom;
-        bytes clientKey;
-        bytes providerKey;
+        bytes32 merkleRoot;
+        bytes32 clientRandom;
+        bytes32 providerRandom;
+        bytes32 clientKey;
+        bytes32 providerKey;
+    }
+
+    function getDummyData() public view returns (bytes memory) {
+        address _client = 0x1111111111111111111111111111111111111111;
+        uint8 _resolverType = 1; // For simplicity
+        address _resolver = 0x2222222222222222222222222222222222222222;
+        address _token = 0x3333333333333333333333333333333333333333;
+        uint256 _terminationTime = block.timestamp + 30 days;
+        bytes32 _details = keccak256(abi.encodePacked("Details for mist invoice"));
+        address _wrappedNativeToken = 0xb16F35c0Ae2912430DAc15764477E179D9B9EbEa; // WETH on sepolia
+        bool _requireVerification = true;
+
+        address _factory = 0xaE57F3689a23792649c597DD3B652b788f0414E4; //sepolia factory
+
+        return abi.encode(
+            _client,
+            _resolverType,
+            _resolver,
+            _token,
+            _terminationTime,
+            _details,
+            _wrappedNativeToken,
+            _requireVerification,
+            _factory
+        );
     }
 
     function run() external {
@@ -22,20 +47,23 @@ contract InvoiceCreationScript is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         // THIS IS THE SEPOLIA MIST POOL, NEEDS TO BE UPDATED TO LINEA-GOERLI
-        MistInvoiceEscrowWrapper mistWrapper = MistInvoiceEscrowWrapper(0xda430a73106D31120Ec5128Bb1008bcd00d25A9C);
+        MistInvoiceEscrowWrapper mistWrapper = MistInvoiceEscrowWrapper(0xd26ffB5e404F5Dc3bF07d9c214957b2731E8EB9b);
 
         // Dummy data
-        MistData memory _mistData = MistData(
-            bytes("dummyMerkleRoot"),
-            bytes("dummyClientRandom"),
-            bytes("dummyProviderRandom"),
-            bytes("dummyClientKey"),
-            bytes("dummyProviderKey")
-        );
+        MistData memory _mistData = MistData({
+            merkleRoot: keccak256(abi.encodePacked("dummyMerkleRoot")),
+            clientRandom: keccak256(abi.encodePacked("dummyClientRandom")),
+            providerRandom: keccak256(abi.encodePacked("dummyProviderRandom")),
+            clientKey: keccak256(abi.encodePacked("dummyClientKey")),
+            providerKey: keccak256(abi.encodePacked("dummyProviderKey"))
+        });
+
         uint256[] memory _amounts = new uint256[](1);
         _amounts[0] = 1000;
-        bytes memory _data = bytes("dummyData");
-        bytes32 _type = bytes32(0x73706c69742d657363726f770000000000000000000000000000000000000000);
+
+        bytes memory _data = getDummyData();
+
+        bytes32 _type = bytes32("escrow");
 
         MistInvoiceEscrowWrapper.MistData memory mistDataForWrapper = MistInvoiceEscrowWrapper.MistData({
             merkleRoot: _mistData.merkleRoot,
@@ -49,7 +77,6 @@ contract InvoiceCreationScript is Script {
 
         console.log("Mist Invoice Address: %s", invoiceAddress);
 
-   
         vm.stopBroadcast();
     }
 }
--- mist-invoice-contracts/script/deployMistInvoiceEscrowWrapper.s.sol
+++ mist-invoice-contracts/script/deployMistInvoiceEscrowWrapper.s.sol
@@ -9,17 +9,17 @@ contract DeployScript is Script {
     function run() external {
         //read env variables and choose EOA for transaction signing
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployerAddress = vm.envAddress("PUBLIC_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
-        // THIS IS THE SEPOLIA MIST POOL, NEEDS TO BE UPDATED TO LINEA-GOERLI
+        // THIS IS THE SEPOLIA MIST POOL, NEEDS TO BE UPDATED TO LINEA-GOERLI. There is currently no mist pool on linea-goerli
         address MIST_POOL = 0x6bA81b91c72755459CfdF3c5ad25eFe636DCd493;
 
         // verified
-        address LINEA_GOERLI_SMART_INVOICE_FACTORY = 0xa9c2372FdFA2ef145A0d13784C74DE96f0e3eaff;
+        address SEPOLIA_SMART_INVOICE_FACTORY = 0xaE57F3689a23792649c597DD3B652b788f0414E4;
+        // address LINEA_GOERLI_SMART_INVOICE_FACTORY = 0xa9c2372FdFA2ef145A0d13784C74DE96f0e3eaff;
 
         // deploy mist invoice escrow
-        MistInvoiceEscrowWrapper mistWrapper = new MistInvoiceEscrowWrapper(LINEA_GOERLI_SMART_INVOICE_FACTORY, MIST_POOL);
+        MistInvoiceEscrowWrapper mistWrapper = new MistInvoiceEscrowWrapper(SEPOLIA_SMART_INVOICE_FACTORY, MIST_POOL);
 
         console.log("MistInvoiceEscrowWrapper Address: %s", address(mistWrapper));
 
