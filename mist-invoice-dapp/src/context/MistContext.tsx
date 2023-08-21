0c060a9c24c3db8343491f0590a604ed403010ed
geovgy <gbh218@gmail.com>
2023-08-20 20:45:45 -0400

    Modify wrapper contract and MistContext

--- mist-invoice-dapp/src/context/MistContext.tsx
+++ mist-invoice-dapp/src/context/MistContext.tsx
@@ -1,8 +1,11 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 
+import { TransferType, UTXONote } from "@usemist/sdk";
+import { IncrementalMerkleTree, MerkleProof } from "@zk-kit/incremental-merkle-tree";
+import { BytesLike, ethers, getBigInt, keccak256 } from "ethers";
+import { poseidon2 } from "poseidon-lite";
 import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
 import { generateKey, randomBytes } from "crypto";
-import { BytesLike } from "ethers";
 import { useIndexedDB } from "react-indexed-db-hook";
 import { logDebug, logError } from "../utils";
 import { Web3Context } from "./Web3Context";
@@ -15,8 +18,21 @@ export type MistData = {
   providerKey?: BytesLike;
 };
 
+export type MistSecret = {
+  merkleRoot?: bigint;
+  providerHash: bigint;
+  clientHash: bigint;
+  encData: BytesLike[];
+};
+
+export const SNARK_SCALAR_FIELD = 21888242871839275222246405745257275088548364400416034343698204186575808495617n;
+export const CLIENT_SIGNAL = getBigInt(keccak256("client")) % SNARK_SCALAR_FIELD;
+export const PROVIDER_SIGNAL = getBigInt(keccak256("provider")) % SNARK_SCALAR_FIELD;
+export const NULLIFYING_KEY = getBigInt(keccak256("nullifier")) % SNARK_SCALAR_FIELD;
+
 export type MistContextType = {
   data?: MistData;
+  secret?: MistSecret;
   loading: boolean;
   getClientProof: () => BytesLike[] | undefined;
 };
@@ -32,8 +48,8 @@ export const MistContextProvider: React.FC<React.PropsWithChildren> = ({
   const { getByID, add } = useIndexedDB(process.env.MIST_DB_NAME || "");
   const { account } = useContext(Web3Context);
   const [data, setData] = useState<MistData>();
-  const [tree, setTree] =
-    useState<StandardMerkleTree<(BytesLike | undefined)[]>>();
+  const [secret, setSecret] = useState<MistSecret>();
+  const [tree, setTree] = useState<IncrementalMerkleTree>();
   const [loading, setLoading] = useState(false);
 
   const providerAddress = process.env.MIST_PROVIDER_ADDRESS || "";
@@ -45,7 +61,8 @@ export const MistContextProvider: React.FC<React.PropsWithChildren> = ({
       [providerAddress, data.providerRandom],
     ];
 
-    const _tree = StandardMerkleTree.of(values, ["address", "bytes32"]);
+    // const _tree = StandardMerkleTree.of(values, ["address", "bytes32"]);
+    const _tree = new IncrementalMerkleTree(poseidon2, 2, 0, 2, values);
     setTree(_tree);
     setData({
       ...data,
@@ -54,34 +71,73 @@ export const MistContextProvider: React.FC<React.PropsWithChildren> = ({
   }, [account, providerAddress, data]);
 
   useEffect(() => {
-    const generate = () => {
-      generateKey("aes", { length: 512 }, (err, key) => {
-        if (err) logError(err);
-        else {
-          const clientKey = key.export().toString("hex");
-          setData({ ...data, clientKey });
-        }
-      });
-      generateKey("aes", { length: 512 }, (err, key) => {
-        if (err) logError(err);
-        else {
-          const providerKey = key.export().toString("hex");
-          setData({ ...data, providerKey });
-        }
-      });
+    // const generate = () => {
+    //   generateKey("aes", { length: 512 }, (err, key) => {
+    //     if (err) logError(err);
+    //     else {
+    //       const clientKey = key.export().toString("hex");
+    //       setData({ ...data, clientKey });
+    //     }
+    //   });
+    //   generateKey("aes", { length: 512 }, (err, key) => {
+    //     if (err) logError(err);
+    //     else {
+    //       const providerKey = key.export().toString("hex");
+    //       setData({ ...data, providerKey });
+    //     }
+    //   });
 
-      const clientRandom = randomBytes(32);
-      const providerRandom = randomBytes(32);
-      const generated = {
-        ...data,
-        clientRandom,
-        providerRandom,
-      };
+    //   const clientRandom = randomBytes(32);
+    //   const providerRandom = randomBytes(32);
+    //   const generated = {
+    //     ...data,
+    //     clientRandom,
+    //     providerRandom,
+    //   };
 
-      setData(generated);
+    //   setData(generated);
 
-      return generated;
+    //   return generated;
+    // };
+    const generate = async () => {
+      const random = getBigInt(randomBytes(32).toString("hex"));
+      const tokenAddress = "0x..."
+      const amounts = [100, 200, 300, 400, 500]; // dummy values
+      // Contains bytes[] of encrypted note, clientKey and providerKey for each amount
+      const encData = await Promise.all(amounts.map(async (amount) => {
+        const note = new UTXONote({
+          sender: account || "",
+          receiver: providerAddress,
+          amount: BigInt(amount),
+          token: tokenAddress,
+          identifier: BigInt(0),
+          random,
+          transferType: TransferType.Deposit,
+          nullifyingKey: NULLIFYING_KEY
+        })
+        return await note.encryptPacked('goerli');
+      }));
+      const clientId = getBigInt(keccak256(ethers.AbiCoder.defaultAbiCoder().encode(["address"],[account])));
+      const providerId = getBigInt(keccak256(ethers.AbiCoder.defaultAbiCoder().encode(["address"],[providerAddress])));
+      const clientHash = poseidon2([poseidon2([clientId, NULLIFYING_KEY]), random]);
+      const providerHash = poseidon2([poseidon2([providerId, NULLIFYING_KEY]), random]);
+  
+      const newSecret = {
+        providerHash,
+        clientHash,
+        encData
+      }
+  
+      setSecret(newSecret);
+      return secret;
     };
+
+    async function genAddRes() {
+      let res = await generate()
+      add(res, account).then(() => {
+        setSecret(res);
+      });
+    }
 
     if (account) {
       setLoading(true);
@@ -92,10 +148,11 @@ export const MistContextProvider: React.FC<React.PropsWithChildren> = ({
             setData(res);
           } else {
             logDebug("Generating new data");
-            res = generate();
-            add(res, account).then(() => {
-              setData(res);
-            });
+            genAddRes();
+            // res = generate();
+            // add(res, account).then(() => {
+            //   setData(res);
+            // });
           }
         });
       } catch (e) {
@@ -117,7 +174,7 @@ export const MistContextProvider: React.FC<React.PropsWithChildren> = ({
   };
 
   return (
-    <MistContext.Provider value={{ data, loading, getClientProof }}>
+    <MistContext.Provider value={{ data, secret, loading, getClientProof }}>
       {children}
     </MistContext.Provider>
   );
